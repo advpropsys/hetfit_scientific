c8696a5d803ad5e5e77f54e6a6992bf9f86223e7
advpropsys <korolev.konstantin.v@gmail.com>
2023-02-22 18:42:49 +0300

    added pinns

--- PINN/pinns.py
+++ PINN/pinns.py
@@ -6,11 +6,23 @@ class PINNd_p(nn.Module):
         weights = tensor([60.,0.5])
         self.weights = nn.Parameter(weights)
     def forward(self,x):
+        """P,U input, d output
+
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         c,b = self.weights
-        x1 = c*(x**b)
+        x1 = (x[0]/(c*x[1]))**0.5
         return x1
     
 class PINNhd_ma(nn.Module):
+    """ h,d -> m_a 
+
+    
+    """
     def __init__(self):
         super(PINNd_p,self).__init__()
         weights = tensor([0.01])
@@ -19,5 +31,21 @@ class PINNhd_ma(nn.Module):
         c, = self.weights
         x1 = c*x[0]*x[1]
         return x1
+    
+class PINNT_ma(nn.Module):
+    """ m_a, U -> T
+
+   
+    """
+    def __init__(self):
+        super(PINNd_p,self).__init__()
+        weights = tensor([0.01])
+        self.weights = nn.Parameter(weights)
+    def forward(self,x):
+        c, = self.weights
+        x1 = c*x[0]*x[1]**0.5
+        return x1
+    
+
     
     
