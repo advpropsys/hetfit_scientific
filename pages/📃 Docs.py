3c72b242744f4aaca2584b7bcaa8c49198e89acf
advpropsys <korolev.konstantin.v@gmail.com>
2023-02-23 19:31:38 +0300

    docs

--- pages/📃 Docs.py
+++ pages/📃 Docs.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.header('Welcome to Docs!')
 with open('docs/nets/envs.html','r+') as f:
     html = f
-st.components.v1.html(html,scrolling=True)
+st.markdown(html,unsafe_allow_html=True)
