84740096c5c1c1324507131b24ae57821d119992
advpropsys <korolev.konstantin.v@gmail.com>
2023-02-23 14:43:49 +0300

    performance super method

--- nets/envs.py
+++ nets/envs.py
@@ -264,6 +264,14 @@ class SCI(): #Scaled Computing Interface
         abs_ape = ape*gen_acc/100
         return {'Generator_Accuracy, %':np.mean(a),'APE_abs, %':abs_ape,'Model_APE, %': ape}
     
+    def performance_super(self,c=0.4,real_data_column_index:tuple = (1,8),real_data_samples:int=23, generated_length:int=1000) -> dict:
+        a=[]
+        for i in range(1000):
+            a.append(100-abs(np.mean(self.df.iloc[1:real_data_samples+1,real_data_column_index[0]:real_data_column_index[1]].values-self.df.iloc[real_data_samples+1:,real_data_column_index[0]:real_data_column_index[1]].sample(real_data_samples).values)/(self.Y.numpy()[1:]+c))*100)
+        gen_acc = np.mean(a)
+        ape = (100-abs(np.mean(self.model(self.X).detach().numpy()-self.Y.numpy()[1:])*100))
+        abs_ape = ape*gen_acc/100
+        return {'Generator_Accuracy, %':np.mean(a),'APE_abs, %':abs_ape,'Model_APE, %': ape}
     
     
 class RCI(SCI): #Real object interface
@@ -381,9 +389,13 @@ class RCI(SCI): #Real object interface
             dfcopy = (self.df.iloc[:,1:8]-self.df.iloc[:,1:8].min())/(self.df.iloc[:,1:8].max()-self.df.iloc[:,1:8].min())
             a.append(100-abs(np.mean(dfcopy.iloc[1:24,1:].values-dfcopy.iloc[24:,1:].sample(23).values)/(dfcopy.iloc[1:24,1:].values+c))*100)
         gen_acc = np.mean(a)
-        ape = (100-abs(np.mean(self.model(self.preds).detach().numpy()-self.Y.numpy())*100))
+        
+        ape = (100-abs(np.mean(self.preds-self.Y.numpy())*100))
         abs_ape = ape*gen_acc/100
         return {'Generator_Accuracy, %':np.mean(a),'APE_abs, %':abs_ape,'Model_APE, %': ape}
+    
+    
+        
     
             
     
