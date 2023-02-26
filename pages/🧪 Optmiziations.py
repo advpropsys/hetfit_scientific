684e17c4bf6eb61399f5fa609541877cffd8b5f6
advpropsys <korolev.konstantin.v@gmail.com>
2023-02-26 16:59:55 +0300

    st

--- pages/🧪 Optmiziations.py
+++ pages/🧪 Optmiziations.py
@@ -1,6 +1,9 @@
 from nets.opti.blackbox import Hyper
 import streamlit as st
 
+
+
+st
 st.code('from nets.opti.blackbox import Hyper')
 st.code('api = Hyper(**kwargs)')
 api = Hyper()
@@ -8,8 +11,9 @@ st.code('api.start_study(n_trials=n,neptune_id,neptune_api)')
 run = None
 def study():
     run = api.start_study(50)
-    st.markdown('### Most recent run:')
+    st.success('Study Finished!',icon='✅')
+    st.markdown('### :orange[Most recent run:]')
     st.write(run)
-if st.button('Start study'):
+if st.button('Start study',use_container_width=True):
     study()
     
