334aaf76a9812af2c67e897a32227f73d7191913
advpropsys <korolev.konstantin.v@gmail.com>
2023-02-27 17:39:28 +0300

    Docs

--- nets/opti/blackbox.py
+++ nets/opti/blackbox.py
@@ -20,21 +20,44 @@ EPOCHS = 10
 N_TRAIN_EXAMPLES = BATCHSIZE * 10
 N_VALID_EXAMPLES = BATCHSIZE * 10
 
+
 class Hyper(SCI):
+    """_summary_
+
+    Args:
+        SCI (_type_): _description_
+    """
     def __init__(self,idx:tuple=(1,3,7),*args, **kwargs):
+        """
+        The function __init__() is a constructor that initializes the class Hyper
+        
+        :param idx: tuple of integers, the indices of the data to be loaded
+        :type idx: tuple
+        """
         super(Hyper,self).__init__()
         self.loader = self.data_flow(idx=idx)
         
     # call self dataflow
     def define_model(self,trial):
+        """
+        We define a function that takes in a trial object and returns a neural network with the number
+        of layers, hidden units and activation functions defined by the trial object.
+        
+        :param trial: This is an object that contains the information about the current trial
+        :return: A sequential model with the number of layers, hidden units and activation functions
+        defined by the trial.
+        """
     # We optimize the number of layers, hidden units and 
         n_layers = trial.suggest_int("n_layers", 2, 6)
         layers = []
 
         in_features = self.input_dim
         
+        # A for loop that iterates over the number of layers in the neural network.
         for i in range(n_layers):
+            # Suggesting the number of hidden units in the ith layer of the neural network.
             out_features = trial.suggest_int("n_units_l{}".format(i), 4, 128)
+            # Suggesting a categorical value for the activation function.
             activations = trial.suggest_categorical('activation',['ReLU','Tanh','SiLU','SELU','RReLU'])
             
             layers.append(nn.Linear(in_features, out_features))
@@ -47,6 +70,16 @@ class Hyper(SCI):
 
         return nn.Sequential(*layers)
     def objective(self,trial):
+        """
+        We define a model, an optimizer, and a loss function. We then train the model for a number of
+        epochs, and report the loss at the end of each epoch
+        
+        *"optimizer": ["Adam", "RMSprop", "SGD",'AdamW','Adamax','Adagrad']*
+        *"lr": (1e-7 1e-3), log=True*
+        
+        :param trial: The trial object that is passed to the objective function
+        :return: The accuracy of the model.
+        """
         # Generate the model.
         model = self.define_model(trial).to(DEVICE)
         mape = R2Score()
@@ -93,6 +126,7 @@ class Hyper(SCI):
                     targs = torch.cat((targs,target))
                     # correct += pred.eq(target.view_as(pred)).sum().item()
             
+            # Calculating the mean absolute percentage error between the predicted and target values.
             accuracy = mape(pred,targs)
 
             trial.report(accuracy, epoch)
@@ -104,6 +138,18 @@ class Hyper(SCI):
         return accuracy
     
     def start_study(self,n_trials:int=100,neptune_project:str=None,neptune_api:str=None):
+        """
+        It takes a number of trials, a neptune project name and a neptune api token as input and runs
+        the objective function on the number of trials specified. If the neptune project and api token
+        are provided, it logs the results to neptune
+        
+        :param n_trials: The number of trials to run, defaults to 100
+        :type n_trials: int (optional)
+        :param neptune_project: the name of the neptune project you want to log to
+        :type neptune_project: str
+        :param neptune_api: your neptune api key
+        :type neptune_api: str
+        """
         
         
         study = optuna.create_study(direction="maximize")
