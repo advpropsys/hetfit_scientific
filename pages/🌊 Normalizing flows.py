772a94d9544d0d1c51655a2e9b11a5678ad8f0a8
advpropsys <korolev.konstantin.v@gmail.com>
2023-03-17 14:54:50 +0300

    normflows

---
+++ pages/🌊 Normalizing flows.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch
+import matplotlib.pyplot as plt
+from nets.normflows import nflow
+import numpy as np
+import seaborn as sns
+import streamlit as st
+
+def random_normal_samples(n, dim=2):
+    return torch.zeros(n, dim).normal_(mean=0, std=3)
+
+st.code('api = nflow(dim=8,latent=16)')
+api = nflow(dim=8,latent=16)
+st.code('api.compile(optim=torch.optim.ASGD,bw=3.05,lr=0.0001,wd=None)')
+api.compile(optim=torch.optim.ASGD,bw=3.05,lr=0.0001,wd=None)
+st.code('api.train(iters=8000)')
+api.train(iters=8000)
+
+
+samples = np.array(api.model.sample(
+        torch.tensor(api.scaled).float()).detach())
+
+g = sns.jointplot(x=samples[:, 0], y=samples[:, 1], kind='kde',cmap=sns.color_palette("Blues", as_cmap=True),fill=True,label='Gaussian KDE')
+
+pl = sns.scatterplot(x=api.scaled[:,0],y=api.scaled[:,1],ax=g.ax_joint,c='orange',marker='+',s=100,label='Real')
+
+st.write(pl)
